0110bfd62fd30e6ed8f7bbd64235d79c91b5f53a
jheremois <jheremy802@gmail.com>
2020-12-23 00:36:57 -0400

    Frontent. Header and About_us section

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,7 +1,6 @@
 const menu = document.getElementById('menu')
 const options = document.getElementById('options')
 
-
 const open_menu = ()=>{
     if(options.style.height !== '210px'){
         options.style.height = '210px'
@@ -10,4 +9,43 @@ const open_menu = ()=>{
     }
 }
 
+const option = document.querySelectorAll('.option')
+
+for (let i = 0; i < option.length; i++) {
+
+    option[i].addEventListener('click', ()=> {
+
+        if(options.style.height === '210px')
+            options.style.height = '0px'
+
+    })
+
+}
+
 menu.addEventListener('click', ()=> open_menu())
+
+/*
+    Header position
+*/
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+
+
+document.addEventListener('scroll', ()=>{
+
+    // if(window.outerWidth < 600){
+
+        if (title.getBoundingClientRect().top < -6) {
+
+            header.style.boxShadow = '0px 3px 9px 0px #09367ebd'
+
+        }
+        else if (title.getBoundingClientRect().top > -3) {
+
+            header.style.boxShadow = 'none'
+
+        }
+
+    // }
+
+})
